ca93b031c15f6d08fa26ab05a4717693a1d16950
barvay <arvaybenedek@gmail.com>
2024-12-01 16:49:45 +0100

    cleanup

--- Day01.kt
+++ Day01.kt
@@ -3,23 +3,12 @@ import kotlin.io.path.readText
 import kotlin.math.abs
 
 fun main() {
-
-    fun part1(input: List<String>) {
-        val (l, r) = input.map { it.split("   ").map(String::toInt) }
-            .map { it[0] to it[1] }
-            .unzip()
-            .let { (l, r) -> l.sorted() to r.sorted() }
-        println(l.indices.sumOf { abs(l[it] - r[it]).toLong() })
-    }
-
-    fun part2(input: List<String>) {
-        val (l, r) = input.map { it.split("   ").map(String::toInt) }
-            .map { it[0] to it[1] }
-            .unzip()
-        println(l.sumOf { x -> x.toLong() * r.count { it == x } })
-    }
-
     val input = Path("input.txt").readText().trim().lines()
-    part1(input)
-    part2(input)
+    val (l, r) = input.map { it.split("   ").map(String::toInt) }
+        .map { it[0] to it[1] }
+        .unzip()
+        .let { (l, r) -> l.sorted() to r.sorted() }
+    println(l.indices.sumOf { abs(l[it] - r[it]).toLong() })
+    val cnt = r.groupingBy { it }.eachCount()
+    println(l.sumOf { x -> x.toLong() * (cnt[x] ?: 0) })
 }
