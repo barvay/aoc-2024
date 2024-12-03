c06bf8284f1143033246b69ccc7ebe20dcbfe5b5
barvay <arvaybenedek@gmail.com>
2024-12-03 06:30:34 +0100

    Day03

---
+++ Day03.kt
@@ -0,0 +1,24 @@
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+
+fun main() {
+    val input = Path("input.txt").readLines()
+    var ans = 0L
+    var enabled = true
+    input.forEach { line ->
+        Regex("""(do\(\)|don't\(\)|mul\(\d+,\d+\))""").findAll(line.trim()).forEach { match ->
+            when (match.value) {
+                "do()" -> enabled = true
+                "don't()" -> enabled = false
+                else -> if (enabled && match.value.startsWith("mul(")) {
+                    val (x, y) = match.value.removePrefix("mul(")
+                        .removeSuffix(")")
+                        .split(',')
+                        .map { it.toInt() }
+                    ans += x * y
+                }
+            }
+        }
+    }
+    println(ans)
+}
