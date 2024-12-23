0bb50ce5cdd43cb0189ab3106a916b6a99647788
Benedek Árvay <arvaybenedek@gmail.com>
2024-12-23 21:52:16 +0100

    Day23

--- Day23.py
+++ Day23.py
@@ -1,16 +1,29 @@
-import networkx as nx
+from utils import *
 
-with open("input.txt") as file:
-    g = nx.Graph(line.strip().split('-') for line in file.readlines())
+g = defaultdict(set)
+with open("input.txt") as f:
+    for line in f:
+        u, v = line.strip().split('-')
+        g[u].add(v)
+        g[v].add(u)
 
-cliques = list(nx.enumerate_all_cliques(g))
-part1 = sum(
-    len(clique) == 3 and any(node.startswith('t') for node in clique)
-    for clique in cliques
-)
-print(part1)
-part2 = max(
-    (','.join(sorted(clique)) for clique in cliques),
-    key=len
-)
-print(part2)
+part1 = set()
+for u in g:
+    for v in g[u]:
+        for w in g[v]:
+            if u in g[w] and any(x.startswith('t') for x in [u, v, w]):
+                part1.add(tuple(sorted([u, v, w])))
+print(len(part1))
+
+
+def bron_kerbosch(r, p, x, g):
+    if not p and not x:
+        yield r
+    else:
+        for v in p.copy():
+            yield from bron_kerbosch(r | {v}, p & g[v], x & g[v], g)
+            p.remove(v)
+            x.add(v)
+
+
+print(max((','.join(sorted(c)) for c in bron_kerbosch(set(), set(g.keys()), set(), g)), key=len))
